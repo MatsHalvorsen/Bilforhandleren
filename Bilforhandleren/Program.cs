b16fa9a1123168e738ee13f371a9e0047b1bd271
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-12-13 13:12:45 +0100

    first commit

---
+++ Bilforhandleren/Program.cs
@@ -0,0 +1,15 @@
+﻿using Bilforhandleren;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Dealership dealership = new Dealership();
+
+        dealership.AddCar(new Car { Brand = "BMW", Year = 2022, RegistrationNumber = "ABC123", Kilometer = 10000 });
+        dealership.AddCar(new Car { Brand = "Audi", Year = 2021, RegistrationNumber = "XYZ789", Kilometer = 15000 });
+
+        dealership.DisplayAvailableCars();
+    }
+}
---
+++ Bilforhandleren/car.cs
@@ -0,0 +1,35 @@
+﻿namespace Bilforhandleren;
+
+public class Car
+{
+    public string? Brand { get; set; }
+    public int Year { get; set; }
+    public string? RegistrationNumber { get; set; }
+    public int Kilometer { get; set; }
+
+    public override string ToString()
+    {
+        return $"Brand: {Brand}\nYear: {Year}\nRegistrationNumber: {RegistrationNumber}\nKilometer: {Kilometer} km\n";
+    }
+
+}
+
+
+    public class Dealership
+{
+    private List<Car> cars = new List<Car>();
+    public void AddCar(Car car)
+    {
+        cars.Add(car);
+    }
+
+
+public void DisplayAvailableCars()
+    {
+        Console.WriteLine("Available cars:");
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
